ccad3c0ba9c3745b28684d7abbb18f5a3ddd2c94
can-cali <can.cali@hotmail.com>
2024-08-17 17:23:07 +0300

    Added LeafNode class and the needed test for the class~

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -9,11 +9,24 @@ class HTMLNode:
         raise NotImplementedError
     
     def props_to_html(self):
-        str = ""
-        for key, value in self.props.items():
-            str = str + " " + key + '="' + value + '" '
-        str = str.strip()
-        return str
+        if not self.props:
+            return ""
+        return " ".join(f'{key}="{value}"' for key, value in self.props.items())
     
     def __repr__(self):
         return f"HTML Object with tag={self.tag}, value={self.value}, childrens={self.children} and props={self.props}"
+
+class LeafNode(HTMLNode):
+    def __init__(self, value, tag=None, props=None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have a non-empty value")
+        if self.tag is None:
+            return self.value
+        else:
+            if self.props is not None and self.props:
+                return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+            else:
+                return f"<{self.tag}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,10 +1,10 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def props(self):
+    def test_props(self):
         node = HTMLNode(
         tag="a",
         value=None,  # Since we're focusing on props, this can be None
@@ -16,6 +16,16 @@ class TestHTMLNode(unittest.TestCase):
         expected_op = 'href="https://www.google.com" target="_blank"'
         assert actual_op == expected_op, f"Expected: {expected_op}, but got: {actual_op}"
 
+    def test_leaf_to_html(self):
+        leaf1 = LeafNode("This is a simple text node")
+        self.assertEqual(leaf1.to_html(), "This is a simple text node")
+
+        leaf2 = LeafNode("This is a paragraph", tag="p")
+        self.assertEqual(leaf2.to_html(), "<p>This is a paragraph</p>")
+    
+    def test_value_error(self):
+        with self.assertRaises(ValueError):
+            LeafNode("").to_html()
 
 if __name__ == "__main__":
     unittest.main()
