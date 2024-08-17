189eeb2fe5b26058a1879de4236b0d770c34e220
can-cali <can.cali@hotmail.com>
2024-08-17 17:55:47 +0300

    added ParentNode and various tests. Fixed some bugs with the help of the guided answers

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,13 +11,16 @@ class HTMLNode:
     def props_to_html(self):
         if not self.props:
             return ""
-        return " ".join(f'{key}="{value}"' for key, value in self.props.items())
+        prop = ""
+        for p in self.props:
+            prop += f' {p}="{self.props[p]}"'
+        return prop
     
     def __repr__(self):
         return f"HTML Object with tag={self.tag}, value={self.value}, childrens={self.children} and props={self.props}"
 
 class LeafNode(HTMLNode):
-    def __init__(self, value, tag=None, props=None):
+    def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
     
     def to_html(self):
@@ -25,8 +28,25 @@ class LeafNode(HTMLNode):
             raise ValueError("All leaf nodes must have a non-empty value")
         if self.tag is None:
             return self.value
-        else:
-            if self.props is not None and self.props:
-                return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
-            else:
-                return f"<{self.tag}>{self.value}</{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("All parent nodes must have a tag")
+        if not self.children:
+            raise ValueError("All parent nodes must have children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+    
