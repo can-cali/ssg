32f1b0c6a1c6a80dff3962a1dc151a60d935e7f0
can-cali <can.cali@hotmail.com>
2024-08-19 22:57:08 +0300

    added extraction funcitonality using regex

---
+++ src/test_extract_md.py
@@ -0,0 +1,21 @@
+import unittest
+from extract_md import (
+    extract_markdown_images,
+    extract_markdown_links,
+)
+
+class TestExtractImages(unittest.TestCase):
+    def test_extract_img(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        result = extract_markdown_images(text)
+        expected = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(result, expected)
+
+    def test_extract_link(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        result = extract_markdown_links(text)
+        expected = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(result, expected)
+
+if __name__ == "__main__":
+    unittest.main()
